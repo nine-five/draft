3d43282572887abdfc1efdd676967de8bb342893
dragon <rongjihuang@gmail.com>
2014-05-09 17:38:35 +0800

    看板水平布局

--- app/kanban/kanban.js
+++ app/kanban/kanban.js
@@ -1,10 +1,52 @@
 jQuery(document).ready(function(){
-  $("body").on("click", ".group a", function(e){
+  // 看板容器
+  var $kb = $(".cram");
+
+  // 新建或删除卡片
+  $(".kb").on("click", ".createCard, .deleteCard", function(e){
     e.stopPropagation();
     e.preventDefault();
-    console.log("create task");
-    $(this).closest(".group").find(".cards")
-      .append('<div class="card">测试标题</div>');
+
+    var $this = $(this);
+    if($this.is(".createCard")){
+      console.log("create card");
+      $this.closest(".group").find(".cards")
+        .append('<div class="card">测试标题</div>');
+    }else{
+      console.log("delete card");
+      $this.closest(".group").find(".card:last").remove();
+    }
+    return false;
+  });
+
+  // 新建分组
+  $(".createGroup").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    console.log("create group");
+
+    $kb.append('<div class="group vlayout unflex">' +
+      ' <div class="header unflex"><h2><i class="fa fa-heart"></i> 测试</h2></div>' +
+      ' <div class="cards autoflex scrollbar">' +
+      '   <div class="card">测试</div>' +
+      '   <div class="card">测试</div>' +
+      ' </div>' +
+      ' <div class="bottom unflex">' +
+      '   <a href="#" class="createCard"><i class="fa fa-plus"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;' +
+      '   <a href="#" class="deleteCard"><i class="fa fa-minus"></i></a>' +
+      ' </div>' +
+      '</div>');
+    $kb.animate({scrollLeft: $kb[0].scrollWidth}, 1000);
+
+    return false;
+  });
+
+  // 删除分组
+  $(".deleteGroup").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    console.log("delete group");
+    $kb.find(".group:last").remove();
     return false;
   });
 
@@ -12,7 +54,6 @@ jQuery(document).ready(function(){
   var ua = navigator.userAgent.toLowerCase();
   if (ua.indexOf("trident") != -1 || ua.indexOf("msie") != -1) {
     console.log("IE browser userAgent = " + navigator.userAgent);
-    var $kb = $(".cram");
     var sh = 0;
     $(window).resize(function resize(){
       console.log("window resize");
